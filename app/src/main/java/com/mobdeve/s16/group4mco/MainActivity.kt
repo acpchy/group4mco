6b2dcb9eb937df51b0069725bb8d182e2acf658d
lavs33 <86014751+lavs33@users.noreply.github.com>
2025-11-17 09:47:05 +0800

    Update MainActivity.kt

--- app/src/main/java/com/mobdeve/s16/group4mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s16/group4mco/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.os.Looper
+import com.mobdeve.s16.group4mco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
