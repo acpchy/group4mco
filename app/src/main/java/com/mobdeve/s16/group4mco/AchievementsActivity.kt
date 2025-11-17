3223223ceaf08356fd09aaebb7e1ad5274df1579
lavs33 <86014751+lavs33@users.noreply.github.com>
2025-11-17 09:36:31 +0800

    Update AchievementsActivity.kt

--- app/src/main/java/com/mobdeve/s16/group4mco/AchievementsActivity.kt
+++ app/src/main/java/com/mobdeve/s16/group4mco/AchievementsActivity.kt
@@ -3,11 +3,16 @@ package com.mobdeve.s16.group4mco
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.s16.group4mco.databinding.ActivityAchievementsBinding
 
 class AchievementsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAchievementsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_achievements)
+        binding = ActivityAchievementsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val achievementsList = findViewById<LinearLayout>(R.id.achievementsList)
 
@@ -23,7 +28,7 @@ class AchievementsActivity : AppCompatActivity() {
             tv.textSize = 16f
             tv.setTextColor(android.graphics.Color.parseColor("#2B2B2B"))
             tv.setPadding(0, 12, 0, 12)
-            achievementsList.addView(tv)
+            binding.achievementsList.addView(tv)
         }
     }
 }
